c4e408045aef2e19ddf08dd2c0d280ebd95a96f1
Eric Hudo <erkinhudo@gmail.com>
2021-03-22 01:21:07 -0400

    https addeed

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require ("express");
+const { Http2ServerResponse } = require("http2");
+const https  =  require ("https");
 
 const app = express();
 
 
 app.get("/",function(req,res){
+    
+    const url = "https://openweathermap.org/weathermap?basemap=map&cities=true&layer=temperature&lat=30&lon=-20&zoom=5"
+    
+    https.get(url,function(response){
+        console.log(response);
+    });
+     
     res.send("Server is up and running");
 });
-
-app.post("/",function(req,res){
-
-    req. 
-})
-
 
 
 
